fa8e8de1fca8a1a93e53cb83c9911b4ec0e854f3
Nick <vinhngalong123@gmail.com>
2025-03-07 16:10:32 +0700

    .

--- Application/Request/UserAccount/UpdateUserRequest.cs
+++ Application/Request/UserAccount/UpdateUserRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +13,7 @@ namespace Application.Request.UserAccount
         public string? LastName { get; set; }
         public string? PhoneNumber { get; set; }
         public string? ImgUrl { get; set; }
+        public Role Role { get; set; }
+
     }
 }
