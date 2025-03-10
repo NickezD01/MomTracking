5a3afa833ffef85ee4c48bb910f9032064d57619
TuanKiet25 <kietntse183810@fpt.edu.vn>
2025-03-10 17:59:49 +0700

    emailnoti

--- Application/Interface/IEmailService.cs
+++ Application/Interface/IEmailService.cs
@@ -10,5 +10,6 @@ namespace Application.Interface
     public interface IEmailService
     {
         Task<ApiResponse> SendValidationEmail(string recievedUser, string emailContent);
+        Task<ApiResponse> SendNotiMail(string recievedUser, string emailContent);
     }
 }
