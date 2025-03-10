03bda69fc7db8fdbd2356742aa5ae8ee31ac0e7b
Nick <vinhngalong123@gmail.com>
2025-03-10 11:07:19 +0700

    .

--- Application/Services/ClaimService.cs
+++ Application/Services/ClaimService.cs
@@ -15,35 +15,23 @@ namespace Application.Services
         }
         public ClaimDTO GetUserClaim()
         {
-            var tokenUserId = _httpContextAccessor.HttpContext!.User.FindFirst("UserId") 
-                              ?? _httpContextAccessor.HttpContext!.User.FindFirst(ClaimTypes.NameIdentifier);
-    
-            var tokenUserRole = _httpContextAccessor.HttpContext!.User.FindFirst(ClaimTypes.Role) 
-                                ?? _httpContextAccessor.HttpContext!.User.FindFirst("Role");
-    
-            var tokenUserName = _httpContextAccessor.HttpContext!.User.FindFirst("FullName")
-                                ?? _httpContextAccessor.HttpContext!.User.FindFirst(ClaimTypes.Name);
-
+            var tokenUserId = _httpContextAccessor.HttpContext!.User.FindFirst("UserId");
+            var tokenUserRole = _httpContextAccessor.HttpContext!.User.FindFirst("Role");
+            var tokenUserName = _httpContextAccessor.HttpContext!.User.FindFirst("FullName");
             if (tokenUserId == null)
             {
                 throw new ArgumentNullException("UserId can not be found!");
             }
-
-            if (tokenUserRole == null)
-            {
-                throw new ArgumentNullException("User Role can not be found!");
-            }
-
-            var userId = Int32.Parse(tokenUserId.Value);
-            Role userRole = Enum.Parse<Role>(tokenUserRole.Value);
-            var fullName = tokenUserName?.Value ?? "Unknown";
-
+            var userId = Int32.Parse(tokenUserId?.Value.ToString()!);
+            Role userRole = Enum.Parse<Role>(tokenUserRole?.Value.ToString()!);
+            var fullName = tokenUserName?.ToString();
             var userClaim = new ClaimDTO
             {
                 Role = userRole,
                 Id = userId,
                 Name = fullName,
             };
+
 
             return userClaim;
         }     
