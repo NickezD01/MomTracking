c1579c9e267a03ef7d3f21bddbe92f859a996ec7
Nick <vinhngalong123@gmail.com>
2025-03-13 13:04:30 +0700

    .

---
+++ Application/Validation/SubPlanValidator.cs
@@ -0,0 +1,20 @@
+﻿using Application.Request.SubscriptionPlan;
+using Application.Request.UserAccount;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validation
+{
+    public class SubPlanValidator : AbstractValidator<CreateSubscriptionPlanRequest>
+    {
+        public SubPlanValidator()
+        {
+            RuleFor(p => (int)p.Name)
+        .InclusiveBetween(0, 2).WithMessage("Role must be between 0 and 2.");
+        }
+    }
+}
